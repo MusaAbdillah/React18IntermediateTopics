8118e5221bf3b4120d2e2eca019dd36eb52242bc
musa <musa.abdillah@ottodigital.id>
2023-12-08 11:27:20 +0700

    state management with zustand

--- src/state-management/counter/Counter.tsx
+++ src/state-management/counter/Counter.tsx
@@ -1,21 +1,25 @@
-import { useReducer } from 'react';
-import CounterReducer from './CounterReducer';
+
+import useCounterStore from './CounterStore';
 
 const Counter = () => {
 
-  const [value, dispatch] = useReducer(CounterReducer, 0)
+  // const [value, dispatch] = useReducer(CounterReducer, 0)
+
+  const {counter, increament, reset} = useCounterStore(); 
 
   return (
     <div>
-      Counter ({value})
+      Counter ({counter})
       <button
-        onClick={() => dispatch({type: 'INCREMENT'})}
+        // onClick={() => dispatch({type: 'INCREMENT'})}
+        onClick={() => increament()}
         className="btn btn-primary mx-1"
       >
         Increment
       </button>
       <button
-        onClick={() => dispatch({type: 'RESET'})}
+        // onClick={() => dispatch({type: 'RESET'})}
+        onClick={() => reset()}
         className="btn btn-primary mx-1"
       >
         Reset
