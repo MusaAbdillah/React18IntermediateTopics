0d26d51e0de54defd4c0fbe1b00ec7a2d66e3776
musa <musa.abdillah@ottodigital.id>
2024-01-04 19:19:19 +0700

    params

--- src/routing/UserDetailPage.tsx
+++ src/routing/UserDetailPage.tsx
@@ -1,4 +1,14 @@
+import { useLocation, useParams } from "react-router";
+import { useSearchParams } from "react-router-dom";
+
 const UserDetailPage = () => {
+  const params = useParams();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const location = useLocation();
+
+  console.log(params);
+  console.log(searchParams.toString());
+  console.log(location);
   return <p>User</p>;
 };
 
